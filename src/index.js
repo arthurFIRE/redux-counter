0f4697050878b639c13f46b29b25e53fc6260104
arthur <arthur@i-on.net>
2020-07-28 14:00:01 +0900

    멀티카운터 만들기

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './containers/App';
 import './index.css';
 
 // Redux 관련 불러오기
@@ -9,7 +9,8 @@ import reducers from './reducers';
 import { Provider } from 'react-redux';
 
 // 스토어 생성
-const store = createStore(reducers);
+// const store = createStore(reducers);
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,28 +1,80 @@
-import number from './number';
-import color from './color';
+import * as types from '../actions/ActionTypes';
 
-import { combineReducers } from 'redux';
-
-/*
-    서브 리듀서들을 하나로 합칩니다.
-    combineReducers 를 실행하고 나면, 나중에 store의 형태가 파라미터로 전달한 객체의 모양대로 만들어집니다.
-    지금의 경우:
+// 초기 상태를 정의합니다.
+const initialState = {
+  counters: [
     {
-        numberData: {
-            number: 0
-        },
-        colorData: {
-            color: 'black'
-        }
+      color: 'black',
+      number: 0
     }
-    로 만들어집니다.
-*/
+  ]
+}
 
+// 리듀서 함수를 정의합니다.
+function counter(state = initialState, action) {
+  // 레퍼런스 생성
+  const { counters } = state;
 
+  switch(action.type) {
+    // 카운터를 새로 추가합니다
+    case types.CREATE:
+      return {
+        counters: [
+          ...counters,
+          {
+            color: action.color,
+            number: 0
+          }
+        ]
+      };
+    // slice 를 이용하여 맨 마지막 카운터를 제외시킵니다
+    case types.REMOVE:
+      return {
+        counters: counters.slice(0, counters.length - 1)
+      };
 
-const reducers = combineReducers({
-  numberData: number,
-  colorData: color
-});
+    // action.index 번째 카운터의 number 에 1 을 더합니다.
+    case types.INCREMENT:
+      console.log("action.index ", action.index);
+      return {
+        counters: [
+          ...counters.slice(0, action.index),
+          {
+            ...counters[action.index],
+            number: counters[action.index].number + 1
+          },
+          ...counters.slice(action.index + 1, counters.length)
+        ]
+      };
 
-export default reducers;
+    // action.index 번째 카운터의 number 에 1 을 뺍니다
+    case types.DECREMENT:
+      return {
+        counters: [
+          ...counters.slice(0, action.index),
+          {
+            ...counters[action.index],
+            number: counters[action.index].number - 1
+          },
+          ...counters.slice(action.index + 1, counters.length)
+        ]
+      };
+
+    // action.index 번째 카운터의 색상을 변경합니다
+    case types.SET_COLOR:
+      return {
+        counters: [
+          ...counters.slice(0, action.index),
+          {
+            ...counters[action.index],
+            color: action.color
+          },
+          ...counters.slice(action.index + 1, counters.length)
+        ]
+      };
+    default:
+      return state;
+  }
+};
+
+export default counter;
